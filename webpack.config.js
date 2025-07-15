ec97d505ca968bcf311b4eb1abf5bedc678012c1
Arsal928 <asheraz7755@gmail.com>
2025-07-15 23:11:31 +0400

    Fix webpack config for Vercel deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,66 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true, // Clean the dist folder before each build
-  },
-  mode: 'development',
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'), // Serve from a public directory
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    entry: './src/index.tsx',
+    output: {
+      filename: isProduction ? '[name].[contenthash].js' : 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+      publicPath: '/',
     },
-    hot: true,
-    open: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: 'ts-loader', // Use ts-loader only for now
+    mode: argv.mode || 'development',
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
       },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                auto: (resourcePath) => resourcePath.includes('.module.'),
-                localIdentName: '[name]__[local]--[hash:base64:5]',
+      hot: true,
+      open: true,
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: 'ts-loader',
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  auto: (resourcePath) => resourcePath.includes('.module.'),
+                  localIdentName: isProduction 
+                    ? '[hash:base64:5]' 
+                    : '[name]__[local]--[hash:base64:5]',
+                },
               },
             },
-          },
-        ],
-      },
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: 'index.html',
+        minify: isProduction,
+      }),
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html', // Path to your template
-      filename: 'index.html',
-    }),
-  ],
+    optimization: isProduction ? {
+      splitChunks: {
+        chunks: 'all',
+      },
+    } : {},
+  };
 };
